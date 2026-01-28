a8956216f180bb7758693124699138355e39eceb
Kath13 <veg19xxr@list.ru>
2026-01-28 11:27:31 +0200

    Decouple from indexes (use fata-attributes)

--- js/tabs.js
+++ js/tabs.js
@@ -1,10 +1,10 @@
 const tabButtons = document.querySelectorAll('.design-list__item');
 const tabDescriptions = document.querySelectorAll('.design__descr');
 const tabImages = document.querySelectorAll('.design-images');
-console.dir(tabImages);
 
-tabButtons.forEach((tabButton, buttonIndex) => {
+tabButtons.forEach((tabButton) => {
     tabButton.addEventListener('click', (event) => {
+        const data = event.target.dataset.tabsHandler;
         tabButtons.forEach(btn => {
             if (btn === event.target) {
                 event.target.classList.add('design-list__item_active');
@@ -12,15 +12,15 @@ tabButtons.forEach((tabButton, buttonIndex) => {
                 btn.classList.remove('design-list__item_active')
             }
         })
-        tabDescriptions.forEach((description, descriptionIndex) => {
-            if (descriptionIndex === buttonIndex) {
+        tabDescriptions.forEach((description) => {
+            if (description.dataset.tabsField === data) {
                 description.classList.remove('hidden');
             } else {
                 description.classList.add('hidden');
             }
         })
-        tabImages.forEach((image, imageIndex) => {
-            if (imageIndex === buttonIndex) {
+        tabImages.forEach((image) => {
+            if (image.dataset.tabsField === data) {
                 image.classList.remove('hidden');
             } else {
                 image.classList.add('hidden');
