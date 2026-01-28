6481f266f20e4861fe377ef669a284a0be2ea587
Kath13 <veg19xxr@list.ru>
2026-01-28 10:17:44 +0200

    Array iteration

--- js/tabs.js
+++ js/tabs.js
@@ -1,9 +1,9 @@
-let mainDirectById = document.getElementById('main');
-let mainById = document.querySelector('#main');
-let mainByClass = document.querySelector('.main');
-let mainByTag = document.querySelector('section');
+let mainDirectById = document.getElementById('main')
+let mainById = document.querySelector('#main')
+let mainByClass = document.querySelector('.main')
+let mainByTag = document.querySelector('section')
 
-let sectionsByTag = document.querySelectorAll('section');
+let sectionsByTag = document.querySelectorAll('section')
 
 console.log('### getElementById', mainDirectById)
 console.log('### querySelector by id', mainById)
@@ -12,3 +12,16 @@ console.log('### querySelector by tag', mainByTag)
 
 console.log('### querySelectorAll by tag', sectionsByTag)
 console.log('### first querySelectorAll by tag', sectionsByTag[0])
+
+for (let i = 0; i < sectionsByTag.length; i++) {
+    console.log(sectionsByTag[i])
+}
+
+console.dir(sectionsByTag)
+
+// Array iteration
+sectionsByTag.forEach((element, index, array) => {
+    console.log('### element:', element)
+    console.log('### index:', index)
+    console.log('### array:', array)
+})
